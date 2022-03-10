b32f5cb470759032f00945f17aba281bb2033ff6
teonino <teonino3627@gmail.com>
2022-03-10 19:00:36 +0100

    Character + m

---
+++ TinyGravity_project/Source/TinyGravity_project/Public/Character_Mov.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actions/PawnActionsComponent.h"
+#include "Character_Mov.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINYGRAVITY_PROJECT_API UCharacter_Mov : public UPawnActionsComponent
+{
+	GENERATED_BODY()
+	
+};
